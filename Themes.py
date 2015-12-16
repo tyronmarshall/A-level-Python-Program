d9b3538b7a883376b2c42bc8b348f44f74362b79
Tyron Marshall <TyronMarshall@icloud.com>
2015-12-16 18:32:06 +0000

    Themes
    Theme file for main program.

---
+++ Themes.py
@@ -0,0 +1,21 @@
+#Themes
+
+sandy_stone_beach = ['#E6E2AF', '#A7A37E',
+                     '#EFECCA', '#046380',
+                     '#002F2F']
+
+flat_ui = ['#2C3E50', '#E74C3C',
+           '#ECF0F1', '#3498DB',
+           '#2980B9']
+
+watermelon = ['#7D8A2E', '#C9D787',
+              '#FFFFFF', '#FFC0A9',
+              '#FFC0A9']
+
+neutral_blue_color = ['#FCFFF5', '#D1DBBD',
+                      '#91AA9D', '#3E606F',
+                      '#193441']
+
+flat_design_colors = ['#334D5C', '#45B29D',
+                      '#EFC94C', '#E27A3F',
+                      '#DF5A49']
